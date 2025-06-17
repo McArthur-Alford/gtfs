179c3bf5dfb9d4826ef5aa28c859b75a15e4a70c
McArthur <mcarthur.alford@proton.me>
2025-06-17 02:51:12 +0000

    Fixed some copies

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
+        };
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain (_: rust);
+      in
+      {
+        devShells.default = craneLib.devShell {
+          RUST_SRC_PATH = "${rust}/lib/rustlib/src/rust/library";
+
+          packages = with pkgs; [
+            rustfmt
+            rust-analyzer-unwrapped
+            rustPackages.clippy
+            rustup
+            cargo-flamegraph
+            rust-script
+            pkg-config
+          ];
+        };
+      }
+    );
+}
